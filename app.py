c260c782febe67e91eecd559fd41b8932fb918c4
PoH-rull <101949468+PoH-rull@users.noreply.github.com>
2023-02-28 17:28:38 +0200

    update

--- app.py
+++ app.py
@@ -1,21 +1,6 @@
 #py -m streamlit run app.py
 import streamlit as st
 tab1, tab2, tab3,tab4 = st.tabs(["מחשבון", "שאלות ותשובות", "קצת עלינו","תרומות"])
-with tab1:
-    st.title("מחשבון")
-with tab2:
-    st.title("שאלות ותשובות")
-with tab3:
-    st.title("עלינו")
-with tab4:
-    st.title("תרומות" )
-    
-    
-def print_results(percent_max):
-    total_price=st.number_input("Enter price")
-    x=total_price+(total_price*percent_max/100)
-    return x
-
 def check_tip():
     if indexRate==15:
         x=print_results(15)
@@ -28,16 +13,32 @@ def check_tip():
     else:
         x=print_results(0)
     return x
-
-
-st.title("percentage calculator")
-st.write("these questions are for how satisfied you are (rate from 1-5 while 1 is the lowest and 5 is the highest)")
-serviceRate=st.slider("how was the service?",max_value=5,min_value=1)
-quickFoodRate=st.slider("how fast the food arrived?",max_value=5,min_value=1)
-totalCost=st.slider("was the cost reasonable?(depending on where you were)",max_value=5,min_value=1)
-indexRate=serviceRate+quickFoodRate+totalCost
-button=st.button("press me when you done",on_click=check_tip())
+with tab1:
+    st.title("מחשבון")
+    st.title("percentage calculator")
+    st.write("these questions are for how satisfied you are (rate from 1-5 while 1 is the lowest and 5 is the highest)")
+    serviceRate=st.slider("how was the service?",max_value=5,min_value=1)
+    quickFoodRate=st.slider("how fast the food arrived?",max_value=5,min_value=1)
+    totalCost=st.slider("was the cost reasonable?(depending on where you were)",max_value=5,min_value=1)
+    indexRate=serviceRate+quickFoodRate+totalCost
+    button=st.button("press me when you done",on_click=check_tip())
 st.write(check_tip())
+with tab2:
+    st.title("שאלות ותשובות")
+with tab3:
+    st.title("עלינו")
+with tab4:
+    st.title("תרומות" )
+    
+    
+def print_results(percent_max):
+    total_price=float(st.number_input("Enter price", key='dasdadasd'))
+    x=total_price+(total_price*percent_max/100)
+    return x
+
+
+
+
 
 
 
